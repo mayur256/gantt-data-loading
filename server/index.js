0ab9ca150b7e5aa5a931de9a9faf878eb6b64fcb
Mayur Upadhayay <mayur123a@gmail.com>
2024-02-01 17:57:55 +0530

    [chore]: Temporarily loading static data

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const fs = require("node:fs");
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { URLSearchParams } = require("node:url");
 
 const app = express()
 const port = 5000;
 
 app.use(cors())
 
-app.get('/GantData', (req, res) => {
-    console.log(req.query)
-    let result = []
+app.get('/GantData', async (req, res) => {
+    const queryParams = new URLSearchParams(req.query);
+    console.log(queryParams)
+    let result = [];
     try {
-        const fileDataStr = fs.readFileSync("./data.json", "utf-8")
-        result = JSON.parse(fileDataStr)
+        await sleep(2000);
+        if (queryParams.get('$expand') === "ExpandingAction,5") {
+            result = JSON.parse(fs.readFileSync("./Task_5.json", "utf-8"))
+        } else if (queryParams.get('$expand') === "ExpandingAction,9") {
+            result = JSON.parse(fs.readFileSync("./Task_9.json", "utf-8"))
+        } else {
+            const fileDataStr = fs.readFileSync("./data.json", "utf-8")
+            result = JSON.parse(fileDataStr)
+        }
     } catch {
         // handle error
     }
@@ -22,3 +31,7 @@ app.get('/GantData', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
+
+function sleep(duration = 1000) {
+    return new Promise(resolve => setTimeout(resolve, duration));
+}
