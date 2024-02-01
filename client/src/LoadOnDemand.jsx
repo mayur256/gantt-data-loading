dc5e8310037e9f357f75a82b6e28b62ae814f615
Mayur Upadhayay <mayur123a@gmail.com>
2024-02-01 17:11:19 +0530

    [chore]: Connecting backend and frontend to fetch data from local server

--- client/src/LoadOnDemand.jsx
+++ client/src/LoadOnDemand.jsx
@@ -4,7 +4,7 @@ import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 const LoadOnDemand = () => {
     const dataSource = new DataManager({
-        url: 'https://services.syncfusion.com/react/production/api/GanttLoadOnDemand',
+        url: 'http://localhost:5000/GantData',
         adaptor: new WebApiAdaptor,
         crossDomain: true
     });
@@ -20,20 +20,32 @@ const LoadOnDemand = () => {
     };
     const projectStartDate = new Date('01/02/2000');
     const projectEndDate = new Date('12/01/2002');
-    return (<div className='control-pane'>
-      <div className='control-section'>
-        <GanttComponent id='LoadOnDemand' dataSource={dataSource} treeColumnIndex={1} taskFields={taskFields} enableVirtualization={true} loadChildOnDemand={false} height='460px' projectStartDate={projectStartDate} projectEndDate={projectEndDate}>
-          <ColumnsDirective>
-            <ColumnDirective field='taskId' width='80'></ColumnDirective>
-            <ColumnDirective field='taskName' headerText='Job Name' width='250' clipMode='EllipsisWithTooltip'></ColumnDirective>
-            <ColumnDirective field='startDate'></ColumnDirective>
-            <ColumnDirective field='duration'></ColumnDirective>
-            <ColumnDirective field='progress'></ColumnDirective>
-          </ColumnsDirective>
-          <Inject services={[Selection, VirtualScroll]}/>
-        </GanttComponent>
-      </div>
+    return (
+        <div className='control-pane'>
+            <div className='control-section'>
+                <GanttComponent
+                    id='LoadOnDemand'
+                    dataSource={dataSource}
+                    treeColumnIndex={1}
+                    taskFields={taskFields}
+                    enableVirtualization={true}
+                    loadChildOnDemand={false}
+                    height='460px'
+                    projectStartDate={projectStartDate}
+                    projectEndDate={projectEndDate}
+                >
+                    <ColumnsDirective>
+                        <ColumnDirective field='taskId' width='80'></ColumnDirective>
+                        <ColumnDirective field='taskName' headerText='Job Name' width='250' clipMode='EllipsisWithTooltip'></ColumnDirective>
+                        <ColumnDirective field='startDate'></ColumnDirective>
+                        <ColumnDirective field='duration'></ColumnDirective>
+                        <ColumnDirective field='progress'></ColumnDirective>
+                    </ColumnsDirective>
+                    <Inject services={[Selection, VirtualScroll]} />
+                </GanttComponent>
+            </div>
 
-    </div>);
+        </div>
+    );
 };
 export default LoadOnDemand;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const fs = require("node:fs");
 const express = require('express');
+const cors = require('cors')
 
 const app = express()
 const port = 5000;
 
+app.use(cors())
+
 app.get('/GantData', (req, res) => {
+    console.log(req.query)
     let result = []
     try {
         const fileDataStr = fs.readFileSync("./data.json", "utf-8")
